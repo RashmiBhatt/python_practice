e86bd8608c62aafe7b532bf9c86005f7b9845de2
RashmiBhatt <rashmibhatt93@gmail.com>
2018-03-04 21:47:43 +0530

    first commit

---
+++ operater.py
@@ -0,0 +1,31 @@
+num1=40
+num2=20
+print("num1+num2 =",num1+num2)
+print("num2-num1 =",num2-num1)
+print("num2*num1 ",num2*num1)
+print("num1/num2  =",num1/num2)
+print("5^4  =",5**4)
+print("22//3    =",22//3 )
+
+print("2347%645",2347%645)
+print("100%7",100%7)
+#assignment operators
+num3=num2+num1
+print(num3)
+#comparison operators  >,<,==,!=
+print("is num3 > num2?",num3>num2)
+print("is num2 == num2?",num3==num2)
+
+
+#logical operators (and,or ,not)
+x=True
+y=False
+print("x and y",x and y)
+print("x or y",x or y)
+print("not of x",not x)
+#membershipe operators it deal with list
+# in =true if find the element in specific sequence
+# not in=True if it not find element in specific sequence
+x=[1,3,5,4,6,5,7,56]
+print("9 in x",9 in x)
+print ("4 in x",4 in x)
